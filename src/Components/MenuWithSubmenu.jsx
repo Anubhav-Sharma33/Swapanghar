59f9c7023094f249304eb86f9f1cd3ee12913b2c
Anubhav Sharma <anubhvsharma33@gmail.com>
2025-04-24 23:43:32 +0530

    den

--- src/Components/MenuWithSubmenu.jsx
+++ src/Components/MenuWithSubmenu.jsx
@@ -64,19 +64,22 @@ export default function MenuWithSubMenu({
         >
           {list.map((cur, i) => {
             let val = "";
-            if (title === "Builder") {
-              val = cur.builderName;
-            } else if (title === "Projects") {
-              val = cur.projectType;
-            } else if (title === "City") {
-              val = cur.name;
-            }else{
-              val = cur.listType;
-            }
+              let href = "";
+              if(title === "Builder"){
+                val = cur.builderName
+                href = "/builder/"+cur.builderName.replace(/\s+/g, "").toLowerCase()
+                }
+                else if(title === "Projects"){
+                val = cur.projectType
+                href = "/projects/"+cur.projectType.replace(/\s+/g, "").toLowerCase()
+                }else if(title === "City"){
+                val = cur.name
+                href = "/city/"+cur.name.replace(/\s+/g, "").toLowerCase()
+                }
             return (
               <li key={i}>
                 <a
-                  href="#"
+                  href={href}
                   className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                 >
                   {val}
